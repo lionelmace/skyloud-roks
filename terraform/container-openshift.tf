85b16b744edb144fad330a90b430982ed2d8d2c9
Lionel Mace <lionel.mace@gmail.com>
2023-10-13 14:38:03 +0200

    retrieved container alb

--- terraform/container-openshift.tf
+++ terraform/container-openshift.tf
@@ -160,13 +160,13 @@ resource "ibm_container_vpc_cluster" "roks_cluster" {
   #name = ibm_container_vpc_cluster.roks_cluster.id
 #}
 
-#data "ibm_container_vpc_alb" "roks_cluster_alb" {
-  #alb_id = data.ibm_container_vpc_cluster.roks_cluster.albs[1].id
-#}
+data "ibm_container_vpc_alb" "roks_cluster_alb" {
+  alb_id = ibm_container_vpc_cluster.roks_cluster.albs[1].id
+}
 
-#output "roks_cluster_alb" {
-  #value = data.ibm_container_vpc_alb.roks_cluster_alb
-#}
+output "roks_cluster_alb" {
+  value = data.ibm_container_vpc_alb.roks_cluster_alb
+}
 
 
 # Additional Worker Pool
